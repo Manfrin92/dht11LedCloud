239a75ec75057e481fee0239e0c363c28c6cbecf
Manfrin92 <manfredd_10@hotmail.com>
2021-06-06 15:15:45 -0300

    fix: turning led off alsoin the begginning

--- ledAndTemp.py
+++ ledAndTemp.py
@@ -8,24 +8,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN_RED, GPIO.OUT)
 GPIO.setup(LED_PIN_YELLOW, GPIO.OUT)
 sensor = Adafruit_DHT.DHT11
-pino_sensor = 2
- 
-print ("*** Lendo os valores de temperatura e umidade");
+pino_sensor = 2 
  
 while(1):
    umid, temp = Adafruit_DHT.read_retry(sensor, pino_sensor);
-   # Caso leitura esteja ok, mostra os valores na tela
    if umid is not None and temp is not None:
      print ("Temperatura = {0:0.1f}  Umidade = {1:0.1f}n").format(temp, umid);
+     GPIO.output(LED_PIN_YELLOW, GPIO.HIGH)
+     GPIO.output(LED_PIN_RED, GPIO.HIGH)
      if temp >= 22:
-         print("Piscando led amarelo")
-         GPIO.output(LED_PIN_RED, GPIO.LOW)
+         print("Piscando led amarelo, temperatura acima/igual de 22 graus celsius")
          GPIO.output(LED_PIN_YELLOW, GPIO.HIGH)
          time.sleep(1)
          GPIO.output(LED_PIN_YELLOW, GPIO.LOW)
      else:
-         print("Piscando led vermelho")
-         GPIO.output(LED_PIN_YELLOW, GPIO.LOW)
+         print("Piscando led vermelho, temperatura abaixo de 22 graus celsius")
          GPIO.output(LED_PIN_RED, GPIO.HIGH)
          time.sleep(1)
          GPIO.output(LED_PIN_RED, GPIO.LOW)
